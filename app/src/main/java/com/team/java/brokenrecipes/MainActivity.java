efc061f0e7256744d22c524d796688fb9f90ad7f
Daniela Tamayo <Daniela Tamayo>
2017-04-06 14:04:32 -0500

    Brackground

--- app/src/main/java/com/team/java/brokenrecipes/MainActivity.java
+++ app/src/main/java/com/team/java/brokenrecipes/MainActivity.java
@@ -2,8 +2,13 @@
 package com.team.java.brokenrecipes;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonPlayVideo2 = (Button)findViewById(R.id.buttonVideo);
+
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+
+        //displays a video file
+        VideoView mVideoView2 = (VideoView)findViewById(R.id.videoView);
+
+        String uriPath2 = "android.resource://com.team.java.brokenrecipes/" + R.raw.background;
+        Uri uri2 = Uri.parse(uriPath2);
+        mVideoView2.setVideoURI(uri2);
+        mVideoView2.requestFocus();
+        mVideoView2.start();
+
+        buttonPlayVideo2.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                VideoView mVideoView2 = (VideoView) findViewById(R.id.videoView);
+                //VideoView mVideoView = new VideoView(this);
+                String uriPath = "android.resource://com.team.java.brokenrecipes/" + R.raw.background;
+                Uri uri2 = Uri.parse(uriPath);
+                mVideoView2.setVideoURI(uri2);
+                mVideoView2.requestFocus();
+                mVideoView2.start();
+            }
+        });
+
     }
 
     // When you click the 'create' button, it launches PostActivity.java
