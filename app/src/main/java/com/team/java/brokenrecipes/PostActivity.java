00d188c9c9c34234ec3ada84cfc4b81590b61fd8
Cynthia Aguillon <caguillon.techie@gmail.com>
2017-04-16 01:58:06 -0500

    set up database code

--- app/src/main/java/com/team/java/brokenrecipes/PostActivity.java
+++ app/src/main/java/com/team/java/brokenrecipes/PostActivity.java
@@ -5,12 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class PostActivity extends AppCompatActivity {
+
+    //to store data
+    private DatabaseReference mDatabase;
+
+    //to store images
+    private FirebaseStorage mStorage;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mStorage = FirebaseStorage.getInstance();
+        storageRef = mStorage.getReferenceFromUrl("gs://broken-recipes-app.appspot.com/");
     }
 
     // When done with PostActivity, it takes you back to MainActivity.java
