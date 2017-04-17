51a291c17ab4f4b5dfd1ff42cffe2cc0aff493a5
J F B <J F B>
2017-04-17 15:39:36 -0500

    added to array adapter

--- app/src/main/java/com/team/java/brokenrecipes/ArrayAdapter.java
+++ app/src/main/java/com/team/java/brokenrecipes/ArrayAdapter.java
@@ -6,28 +6,37 @@ import static com.team.java.brokenrecipes.R.id.listView;
  * Created by Cynthia on 4/3/2017.
  */
 
-
 /*
 frank will be working on the array adapter for our app
-
 */
 
 
-/*
-public class ArrayAdapter
-{
-    ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-
-    ListView listview = (ListView)findViewById(R.id.lvItems);
-    listView.setAdapter();
 
 
 
+public class UsersAdapter extends ArrayAdapter<User> {
+    public UsersAdapter(Context context, ArrayList<User> users) {
+        super(Context, 0, users);
+    }
 
-
+  //  @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        User user = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_feed, parent, false);
+        }
+        // Lookup view for data population
+        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+        TextView tvHome = (TextView) convertView.findViewById(R.id.tvHome);
+        // Populate the data into the template view using the data object
+        tvName.setText(user.name);
+        tvHome.setText(user.hometown);
+        // Return the completed view to render on screen
+        return convertView;
+    }
 }
-
-*/
 
 
 
