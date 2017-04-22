0381737004b7e7dd7abba3e005652355643a2f08
Cynthia Aguillon <caguillon.techie@gmail.com>
2017-04-22 18:55:34 -0500

    started view post set up

---
+++ app/src/main/java/com/team/java/brokenrecipes/ViewPostActivity.java
@@ -0,0 +1,13 @@
+package com.team.java.brokenrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_post);
+    }
+}
