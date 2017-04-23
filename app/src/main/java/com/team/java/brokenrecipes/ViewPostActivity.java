de896e85bef0f24974566861694b42c136c6d66e
Cynthia Aguillon <caguillon.techie@gmail.com>
2017-04-22 19:12:12 -0500

    test viewPost

--- app/src/main/java/com/team/java/brokenrecipes/ViewPostActivity.java
+++ app/src/main/java/com/team/java/brokenrecipes/ViewPostActivity.java
@@ -1,13 +1,28 @@
 package com.team.java.brokenrecipes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.team.java.brokenrecipes.Models.Post;
 
 public class ViewPostActivity extends AppCompatActivity {
+
+    private Post post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_post);
+
+        TextView tvRTitle = (TextView) findViewById(R.id.tvRTitle);
+        TextView tvRTime = (TextView) findViewById(R.id.tvRTime);
+        TextView tvRName = (TextView) findViewById(R.id.tvRName);
+        TextView tvRRecipe = (TextView) findViewById(R.id.tvRRecipe);
+
+        tvRTitle.setText(post.title);
+        tvRTime.setText(post.time);
+        tvRName.setText(post.name);
+        tvRRecipe.setText(post.recipe);
     }
 }
