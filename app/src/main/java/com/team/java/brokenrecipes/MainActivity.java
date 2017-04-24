4f072236ab20471e6b89d7332fb5c65020bf923a
Cynthia Aguillon <caguillon.techie@gmail.com>
2017-04-24 15:44:00 -0500

    deleted some comments

--- app/src/main/java/com/team/java/brokenrecipes/MainActivity.java
+++ app/src/main/java/com/team/java/brokenrecipes/MainActivity.java
@@ -41,45 +41,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
-        //******displays a video file as background(transparent button)
-        /*Button buttonPlayVideo2 = (Button)findViewById(R.id.buttonVideo);
-        getWindow().setFormat(PixelFormat.UNKNOWN);
-
-        VideoView mVideoView2 = (VideoView)findViewById(R.id.videoView);
-
-        String uriPath2 = "android.resource://com.team.java.brokenrecipes/" + R.raw.background;
-        Uri uri2 = Uri.parse(uriPath2);
-        mVideoView2.setVideoURI(uri2);
-        mVideoView2.requestFocus();
-        mVideoView2.start();
-
-        buttonPlayVideo2.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                VideoView mVideoView2 = (VideoView) findViewById(R.id.videoView);
-                //VideoView mVideoView = new VideoView(this);
-                String uriPath = "android.resource://com.team.java.brokenrecipes/" + R.raw.background;
-                Uri uri2 = Uri.parse(uriPath);
-                mVideoView2.setVideoURI(uri2);
-                mVideoView2.requestFocus();
-                mVideoView2.start();
-            }
-        });
-
-        //******moves buttons when screen is touched
-        recipesLayout = (ViewGroup) findViewById(R.id.recipesLayout);
-
-        recipesLayout.setOnTouchListener(
-                new RelativeLayout.OnTouchListener(){
-                    @Override
-                    public boolean onTouch(View v, MotionEvent event){
-                        moveButton1();  //moves create button to center
-                        moveButton2();  //moves browse button to center
-                        return false;
-                    }
-                }
-        );*/
     }
 
     // When you click the 'create' button, it launches PostActivity.java
@@ -157,58 +118,4 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     public void onBackPressed() {
 
     }
-
-    //CREATE
-    /*public void moveButton1(){
-        View btnCreate = findViewById(R.id.btnCreate);
-        /************************************************************
-        <uses-sdk android:minSdkVersion="14"
-        android:targetSdkVersion="20"
-        android:maxSdkVersion="21" />
-        *************************************************************
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            TransitionManager.beginDelayedTransition(recipesLayout);
-        }
-
-        //change position of button
-        RelativeLayout.LayoutParams positionRules = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        positionRules.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        positionRules.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        positionRules.addRule(RelativeLayout.ALIGN_PARENT_END, RelativeLayout.TRUE);
-        btnCreate.setLayoutParams(positionRules);
-
-        //change size of button
-        ViewGroup.LayoutParams sizeRules = btnCreate.getLayoutParams();
-        sizeRules.width = 180;
-        sizeRules.height = 100;
-        btnCreate.setLayoutParams(sizeRules);
-    }
-
-    //BROWSE
-    public void moveButton2(){
-        View btnBrowse = findViewById(R.id.btnBrowse);
-        ************************************************
-        <uses-sdk android:minSdkVersion="14"
-        android:targetSdkVersion="20"
-        android:maxSdkVersion="21" />
-        ************************************************
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            TransitionManager.beginDelayedTransition(recipesLayout);
-        }
-
-        //change position of button
-        RelativeLayout.LayoutParams positionRules = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        positionRules.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        positionRules.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-        positionRules.addRule(RelativeLayout.ALIGN_PARENT_END, RelativeLayout.TRUE);
-        btnBrowse.setLayoutParams(positionRules);
-
-        //change size of button
-        ViewGroup.LayoutParams sizeRules = btnBrowse.getLayoutParams();
-        sizeRules.width = 180;
-        sizeRules.height = 100;
-        btnBrowse.setLayoutParams(sizeRules);
-    }*/
 }
